91b0c95dab5b5d1219e3dd9ae40e65ccc0d0f7e0
Yashsavani-scet <yashsavani.it22@scet.ac.in>
2025-03-17 18:18:22 +0530

    api test

--- client/src/api/axiosInstance.js
+++ client/src/api/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:3000", // Use Vite's env variable
+  baseURL: import.meta.env.VITE_API_BASE_URL , // Use Vite's env variable
   withCredentials: true, // Send cookies with requests
 });
 
