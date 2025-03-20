15f4f30d3d57b33e432c3ec6393ff4d087f0b080
Yashsavani-scet <yashsavani.it22@scet.ac.in>
2025-03-20 18:19:00 +0530

    backend 1

--- server/models/User.js
+++ server/models/User.js
@@ -1,31 +1,12 @@
-// import mongoose from "mongoose";
-
-// const UserSchema = new mongoose.Schema({
-//   userName: { type: String, required: true, unique: true, index: true },
-//   userEmail: { type: String, required: true, unique: true, index: true },
-//   password: { type: String, required: true },
-//   role: { type: String, enum: ["user", "admin", "instructor"], default: "user" },
-// }, { timestamps: true });
-
-// const User = mongoose.model("User", UserSchema);
-// export default User;
-
 import mongoose from "mongoose";
-import bcrypt from "bcryptjs";
 
-const userSchema = new mongoose.Schema({
-    userName: { type: String, required: true, unique: true,index: true},
-    userEmail: { type: String, required: true, unique: true ,index: true},
-    password: { type: String, required: true },
-    role: { type: String, enum: ["user", "admin", "instructor"], default: "user" },
-});
+const UserSchema = new mongoose.Schema({
+  userName: { type: String, required: true, unique: true, index: true },
+  userEmail: { type: String, required: true, unique: true, index: true },
+  password: { type: String, required: true },
+  role: { type: String, enum: ["user", "admin", "instructor"], default: "user" },
+}, { timestamps: true });
 
-// Hash password before saving
-userSchema.pre("save", async function (next) {
-    if (!this.isModified("password")) return next();
-    this.password = await bcrypt.hash(this.password, 10);
-    next();
-});
-
-const User = mongoose.model("User", userSchema);
+const User = mongoose.model("User", UserSchema);
 export default User;
+
