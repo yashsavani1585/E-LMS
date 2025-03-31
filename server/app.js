a418f93b38968fed3de40e53cfc0e7bb9929f1eb
yash savani <yashsavani1585@gmail.com>
2025-03-31 12:49:26 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -29,7 +29,7 @@ app.use(
     origin: [
       "http://localhost:5173",
       "https://e-lms-1.onrender.com",
-      "https://e-lms-ruby.vercel.app"
+      "https://e-lms-pearl.vercel.app"
     ],
     credentials: true,
     methods: ["GET", "POST", "DELETE", "PUT"],
