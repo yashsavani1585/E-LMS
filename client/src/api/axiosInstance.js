2e5ab7a4842ff77124848058321c1e58e42cd74e
yash savani <yashsavani1585@gmail.com>
2025-05-30 12:59:28 +0530

    Update axiosInstance.js

--- client/src/api/axiosInstance.js
+++ client/src/api/axiosInstance.js
@@ -1,26 +1,53 @@
+// import axios from 'axios';
+
+
+// const axiosInstance = axios.create({
+//   baseURL: "https://e-lms-vb12.onrender.com" || "http://localhost:3000",
+//   withCredentials: true, 
+// });
+
+// axiosInstance.interceptors.request.use(
+//     (config) => {
+//       try {
+//         const accessToken = JSON.parse(sessionStorage.getItem("accessToken")) || "";
+        
+//         if (accessToken) {
+//           config.headers.Authorization = `Bearer ${accessToken}`;
+//         }
+//       } catch (error) {
+//         console.error("Error retrieving accessToken from sessionStorage", error);
+//       }
+      
+//       return config;
+//     },
+//     (err) => Promise.reject(err)
+// );
+
+// export default axiosInstance;
+
 import axios from 'axios';
 
-
 const axiosInstance = axios.create({
-  baseURL: "https://e-lms-vb12.onrender.com" || "http://localhost:3000",
-  withCredentials: true, 
+  baseURL:"https://e-lms-vb12.onrender.com"
+  withCredentials: true, // Send cookies with requests
 });
 
 axiosInstance.interceptors.request.use(
-    (config) => {
-      try {
-        const accessToken = JSON.parse(sessionStorage.getItem("accessToken")) || "";
-        
-        if (accessToken) {
-          config.headers.Authorization = `Bearer ${accessToken}`;
-        }
-      } catch (error) {
-        console.error("Error retrieving accessToken from sessionStorage", error);
+  (config) => {
+    try {
+      const accessToken = sessionStorage.getItem("accessToken") || "";
+
+      if (accessToken) {
+        config.headers.Authorization = `Bearer ${accessToken}`;
       }
-      
-      return config;
-    },
-    (err) => Promise.reject(err)
+    } catch (error) {
+      console.error("Error retrieving accessToken from sessionStorage", error);
+    }
+
+    return config;
+  },
+  (err) => Promise.reject(err)
 );
 
 export default axiosInstance;
+
