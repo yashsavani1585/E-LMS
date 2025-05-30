543e133a447e71eb480c994628871c73a3ab3660
yash savani <yashsavani1585@gmail.com>
2025-05-30 12:20:43 +0530

    Update axiosInstance.js

--- client/src/api/axiosInstance.js
+++ client/src/api/axiosInstance.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   baseURL: "https://e-lms-vb12.onrender.com" || "http://localhost:3000",
-  withCredentials: true, // Send cookies with requests
+  withCredentials: true, 
 });
 
 axiosInstance.interceptors.request.use(
